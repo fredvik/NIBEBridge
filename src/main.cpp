0097df50bd13e880363f7ac270cf0313c6cb4cfa
Fredrik Viklund <fredrik@viklund.se>
2018-05-22 22:23:39 +0200

    Added prototyped statemachine code from  january

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,169 @@
 
 #include <Arduino.h>
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
-// #define LED_BUILTIN 13
+/*
+ * State machine protocol to read data from Nibe 360P
+ * v0.1 - January 2018
+**/
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+/*
+ * telegram = hela paketet från eller till RCU
+ * message  = listan av C0, length, parametrar/värden - som beräknar checksum
+**/
+
+// Refactor till en klass?
+
+typedef void *(*StateFunc)();
+StateFunc statefunc, nextstate;
+
+char newchar;
+char messagelength;     
+
+// States must be prototype declared
+void *idle();
+void *adressbegun();
+void *adressed();
+void *getsender();
+void *getlength();
+void *getregisterhigh();
+void *getregisterlow();
+void *getvaluehigh();
+void *getvaluelow();
+void *checktelegram();
+void *endoftelegram();
+
+// Functions/states in the statemachine
+void *idle() {
+    if (newchar == 0x00) {
+        nextstate = addressbegun;
+    }
+    else {
+        // nextstate = idle;    // There may be stray bytes with MARK parity that are not addresses
+    }
+
+    return (void*) nextstate;   // Need to cast into correct function pointer type
+    // https://stackoverflow.com/questions/42951696/error-invalid-conversion-from-void-to-void-in-case-of-dlsysm
 }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  // wait for a second
-  delay(1500);
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-
-   // wait for a second
-  delay(200);
+void *addressbegun() {
+    if (newchar == 0x14) {
+        nextstate = addressed;
+    }
+    else {
+        nextstate = idle;       // Message is not for us
+    }
+    return (void*) nextstate;
 }
+
+void *adressed() {
+    if (newchar == 0xC0) {      // Command byte(?)
+        checksum = newchar;
+        nextstate = getsender;
+    }
+    else {
+        nextstate = idle;       // Error - consider logging
+    }
+    return (void*) nextstate;
+}
+
+void *getsender() {
+    if (newchar == 0x24) {      // Sender is the NIBE controller
+        checksum ^= newchar;
+        nextstate = getlength;
+    }
+    else {
+        nextstate = idle;       // Error - unkonwn sender
+    }
+    return (void*) nextstate;
+}
+
+void *getlength() {
+    if (newchar <> 0x00) {      // Command byte(?)
+        messagelength = newchar;
+        checksum ^= newchar;
+        nextstate = getregisterhigh;
+    }
+    else {
+        nextstate = error;       // Error - consider logging
+    }
+    return (void*) nextstate;
+}
+
+void *getregisterhigh() {
+    if (newchar == 0x00) {      // First byte of register is always 0x00
+        checksum ^= newchar;
+        nextstate = getregisterlow;
+    }
+    else {
+        nextstate = error;       // Error - consider logging
+    }
+    return (void*) nextstate;
+}
+
+void *getregisterlow() {
+    paramno = newchar;
+    checksum ^= newchar;
+    if (getparamlength(paramno) == 1) {
+        nextstate = getvaluelow;
+    }
+    else {
+        nextstate = getvaluehigh;    
+    }
+    return (void*) nextstate;
+}
+
+void *getvaluehigh() {
+    // Byte manipulation from https://stackoverflow.com/questions/13900302/set-upper-and-lower-bytes-of-an-short-int-in-c
+    paramval = (paramval & 0x00FF) | (newchar << 8);    // Set the high byte of the parameter value
+    checksum ^= newchar;
+    nextstate = getvaluelow;
+}
+
+void *getvaluelow() {
+    paramval = (paramval & 0xFF00) | newchar;           // Set the low byte of the parameter value
+    checksum ^= newchar;
+    nextstate = getvaluelow;
+}
+
+void *checktelegram() {
+    if (newchar == checksum) {
+        sendack();               // Confirm to NIBE that message was not corrupt
+        nextstate = error;    
+    }
+    else {
+        sendack();               // Always send ACK to NIBE to avoid alarms
+        nextstate = error;    
+    }
+}
+
+void *endoftelegram() {
+    if (newchar == 0x03) {       // ETX received
+        nextstate = idle;
+    }
+    else {
+        nextstate = error;
+    }
+    
+}
+
+void *error() {
+}
+
+
+
+void setup() {
+    // put your setup code here, to run once: 
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+
+    // while input is available from RS485 input, read newchar
+    // Call statemachine and use the char
+    statefunc = (StateFunc)(*statefunc)();
+
+    // if (msgreceived != '') {
+        
+
+}
+
